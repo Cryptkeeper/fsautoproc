c0d7d867afdd1f128acfa51df396cab433de3146
Nick Krecklow <hello@github.nklow.com>
2024-06-27 16:18:08 -0500

    include source file/line number when logging errors

--- include/log.h
+++ include/log.h
@@ -1,8 +1,9 @@
 #ifndef FSAUTOPROC_LOG_H
 #define FSAUTOPROC_LOG_H
 
-#define log_info(fmt, ...) printf(fmt "\n", __VA_ARGS__)
-#define log_verbose(fmt, ...) printf(fmt "\n", __VA_ARGS__)
-#define log_error(fmt, ...) fprintf(stderr, fmt "\n", __VA_ARGS__)
+#define log_info(fmt, ...) printf(fmt " \n", __VA_ARGS__)
+#define log_verbose(fmt, ...) printf(fmt " \n", __VA_ARGS__)
+#define log_error(fmt, ...)                                                    \
+  fprintf(stderr, fmt " [%s:%d]\n", __VA_ARGS__, __FILE_NAME__, __LINE__)
 
 #endif//FSAUTOPROC_LOG_H
