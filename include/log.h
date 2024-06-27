b780768dc9464838e751ea39e20d8f7297b430a0
Nick Krecklow <hello@github.nklow.com>
2024-06-27 16:39:17 -0500

    use `__FILE__` if `__FILE_NAME__` is not defined

--- include/log.h
+++ include/log.h
@@ -1,9 +1,16 @@
 #ifndef FSAUTOPROC_LOG_H
 #define FSAUTOPROC_LOG_H
 
+// use __FILE_NAME__ if available for a shorter log message
+#ifdef __FILE_NAME__
+#define LOG_FILE_MACRO __FILE_NAME__
+#else
+#define LOG_FILE_MACRO __FILE__
+#endif
+
 #define log_info(fmt, ...) printf(fmt " \n", __VA_ARGS__)
 #define log_verbose(fmt, ...) printf(fmt " \n", __VA_ARGS__)
 #define log_error(fmt, ...)                                                    \
-  fprintf(stderr, fmt " [%s:%d]\n", __VA_ARGS__, __FILE_NAME__, __LINE__)
+  fprintf(stderr, fmt " [%s:%d]\n", __VA_ARGS__, LOG_FILE_MACRO, __LINE__)
 
 #endif//FSAUTOPROC_LOG_H
