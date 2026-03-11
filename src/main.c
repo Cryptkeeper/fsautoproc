d7956276dd5764643b8a501f47b78b0330fd7749
Nick Krecklow <hello@github.nklow.com>
2026-03-10 19:18:39 -0500

    silence unchecked `indexput` return value warnings

--- src/main.c
+++ src/main.c
@@ -330,7 +330,7 @@ static bool filterjunk(const char* fp) {
     if (initargs.verbose) log_info("[j] %s", fp);
   } else {
     const struct fsstat_s st = {0};
-    indexput(&goodmap, fp, fphash, &st, 0);// mark as known good
+    (void)indexput(&goodmap, fp, fphash, &st, 0);// mark as known good
   }
   return junk;
 }
